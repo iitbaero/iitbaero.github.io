eb0c8c9d7d04eb47664e179711376ea6911433b9
Raj Krishnan <rajkrishnan1996@gmail.com>
2017-06-18 18:41:07 +0530

    Added debugging code

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -15,8 +15,15 @@ touch .nojekyll
 
 # Copy generated files here
 cd -
+
+ls -al
+ls -al _site/
+
 cp -r _site/* ../iitbaero.github.io.master/
 cd -
+
+ls -al
+
 
 git config user.email ${GH_EMAIL}
 git config user.name "Deployment Bot"
